9c4530ee1b7e48057cd16920654a4bffcbb660a2
Cassim <111745304+catsums@users.noreply.github.com>
2023-11-16 09:11:28 +0200

    Added CJS

---
+++ lib/cjs/MY.d.ts
@@ -0,0 +1,4 @@
+export declare function mod(n: number, m: number): number;
+export declare function safeDivide(a: number, b: number, useNaN?: boolean): number;
+export declare function isInfinity(x: number): boolean;
+export declare function roundTo(num: any, step: any): any;
